6dffcc544890d88e74fdbad00030eeaa51408a30
kirilllukashev <kirillukashev@yandex.ru>
2023-03-21 01:57:09 +0300

    Add under development class company

--- Company/Company.cpp
+++ Company/Company.cpp
@@ -1,1 +1,53 @@
 #include "Company.h"
+
+Company::Company() {}
+
+Company::Company(std::string name, std::string ticker, std::string category, double open_price, double close_price, double low_price, double high_price) {
+  std::transform(name.begin(), name.end(), name.begin(), ::toupper);
+  this->name_ = name;
+  std::transform(category.begin(), category.end(), category.begin(), ::toupper);
+  this->category_index_ = GetCategoryIndex(category);
+  this->stock_ = Stock(ticker, open_price, close_price, low_price, high_price);
+}
+
+int Company::GetCategoryIndex(std::string s) {
+  for (int i = 0; i < category_list.size(); ++i) {
+    if (s == category_list[i]) {
+      return i;
+    }
+  }
+}
+
+std::string Company::GetName() {
+  return this->name_;
+}
+
+void Company::SetName(std::string name) {
+  this->name_ = name;
+}
+
+int Company::GetCategoryIndex() {
+  return this->category_index_;
+}
+
+void Company::SetCategoryIndex(int category_index) {
+  this->category_index_ = category_index;
+}
+
+Stock Company::GetStock() {
+  return this->stock_;
+}
+
+void Company::SetStock(Stock stock) {
+  this->stock_ = stock;
+}
+
+std::string Company::GetCategory() {
+  return category_list[this->category_index_];
+}
+
+std::string Company::ToString() {
+  return "6";
+  //return "Сompany{name = '" + GetName() + "', category = '" + GetCategory() +
+  //"', stock = " + this->stock_.ToString();
+}
--- Company/Company.h
+++ Company/Company.h
@@ -1,21 +1,26 @@
 #pragma once
-#include<string>
+#include <string>
+#include <vector>
+#include <algorithm>
 #include "Stock.h"
-#include<vector>
 
 std::vector<std::string> category_list = {"ENERGY", "MATERIALS", "INDUSTRY", "FINANCE", "IT"};
 
 class Company {
  public:
+  Company();
   Company(std::string name, std::string ticker, std::string category, double open_price,
           double close_price, double low_price, double high_price);
-  int GetCategoryIndex(std::string s) {
-    for (int i = 0; i < category_list.size(); ++i) {
-      if (s == category_list[i]) {
-        return i;
-      }
-    }
-  }
+  int GetCategoryIndex(std::string s);
+
+  std::string GetName();
+  void SetName(std::string name);
+  int GetCategoryIndex();
+  void SetCategoryIndex(int category_index);
+  Stock GetStock();
+  void SetStock(Stock stock);
+  std::string GetCategory();
+  std::string ToString();
  private:
   std::string name_;
   int category_index_;
