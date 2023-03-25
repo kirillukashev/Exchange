e90f5fb4345a25e99b82796740da8ba7f06b5c2d
kirilllukashev <kirillukashev@yandex.ru>
2023-03-25 13:05:23 +0300

    Change .gitignore and CMakeList

---
+++ Company/CompanyActionHandler.h
@@ -0,0 +1,16 @@
+//
+// Created by User on 25.03.2023.
+//
+
+#ifndef EXCHANGE_COMPANYACTIONHANDLER_H
+#define EXCHANGE_COMPANYACTIONHANDLER_H
+
+
+
+class CompanyActionHandler {
+
+};
+
+
+
+#endif //EXCHANGE_COMPANYACTIONHANDLER_H
