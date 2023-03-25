6aae5a13f76c21e9a60358e54b5726c9337acdd2
kirilllukashev <kirillukashev@yandex.ru>
2023-03-25 13:23:29 +0300

    Change CompanyActionHandler.h

--- Company/CompanyActionHandler.h
+++ Company/CompanyActionHandler.h
@@ -1,16 +1,3 @@
-//
-// Created by User on 25.03.2023.
-//
-
-#ifndef EXCHANGE_COMPANYACTIONHANDLER_H
-#define EXCHANGE_COMPANYACTIONHANDLER_H
-
-
-
 class CompanyActionHandler {
 
 };
-
-
-
-#endif //EXCHANGE_COMPANYACTIONHANDLER_H
